a0ae74e2587a03009cb7e645dd9487726992e8fd
huy9797 <huyphamquang1997@gmail.com>
2022-07-15 00:08:43 +0700

    yoda

--- app/src/main/java/com/example/exoplayerdemo/MainActivity.kt
+++ app/src/main/java/com/example/exoplayerdemo/MainActivity.kt
@@ -1,11 +1,147 @@
 package com.example.exoplayerdemo
 
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.MediaMetadata
+import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.source.ConcatenatingMediaSource
+import com.google.android.exoplayer2.source.MediaSource
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.ui.PlayerView
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.exoplayer2.util.Util
+import com.google.common.collect.ImmutableList
+import java.util.concurrent.TimeUnit
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), Player.Listener {
+
+    private lateinit var player: ExoPlayer
+    private lateinit var playerView: PlayerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        setupPlayer()
+//        addMP3()
+        addMP4Files()
+
+
+        // restore playstate on Rotation
+        if (savedInstanceState != null) {
+            if (savedInstanceState.getInt("mediaItem") != 0) {
+                val restoredMediaItem = savedInstanceState.getInt("mediaItem")
+                val seekTime = savedInstanceState.getLong("SeekTime")
+//                player.seekTo(restoredMediaItem, seekTime)
+                player.play()
+            }
+        }
+    }
+
+    private fun addMP4Files() {
+        val mediaItem = MediaItem.fromUri(getString(R.string.media_url_mp4))
+        val mediaItem2 = MediaItem.fromUri(getString(R.string.media_url_mp4))
+        val newItems: List<MediaItem> = ImmutableList.of(
+            mediaItem,
+            mediaItem2
+        )
+        player.addMediaItems(newItems)
+        player.prepare(
+            /*buildMediaSource(
+                listOf<Uri>(
+                    Uri.parse(getString(R.string.media_url_mp4)),
+                    Uri.parse(getString(R.string.media_url_mp4))
+                )
+            )*/
+        )
+    }
+
+    private fun buildMediaSource(uris: List<Uri>): ConcatenatingMediaSource {
+        val userAgent = Util.getUserAgent(this, "MusicPlayer")
+        val defaultMediaSource = DefaultDataSourceFactory(this, userAgent)
+        val progressiveMediaSource = ProgressiveMediaSource.Factory(defaultMediaSource)
+        val mediaSources = ArrayList<MediaSource>()
+
+        for (uri in uris) {
+            mediaSources.add(progressiveMediaSource.createMediaSource(uri))
+        }
+
+        val concatenatingMediaSource = ConcatenatingMediaSource()
+        concatenatingMediaSource.addMediaSources(mediaSources)
+
+        return concatenatingMediaSource
+    }
+
+    private fun setupPlayer() {
+        player = ExoPlayer.Builder(this).build()
+        playerView = findViewById(R.id.video_view)
+        playerView.setShowMultiWindowTimeBar(true)
+        playerView.player = player
+        player.addListener(this)
+    }
+
+    private fun addMP3() {
+        // Build the media item.
+        val mediaItem = MediaItem.fromUri(getString(R.string.test_mp3))
+        player.setMediaItem(mediaItem)
+        // Set the media item to be played.
+        player.setMediaItem(mediaItem)
+        // Prepare the player.
+        player.prepare()
+    }
+
+
+    override fun onStop() {
+        super.onStop()
+        player.release()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        setupPlayer()
+//        addMP3()
+        addMP4Files()
+    }
+
+    // handle loading
+    override fun onPlaybackStateChanged(state: Int) {
+        when (state) {
+            Player.STATE_BUFFERING -> {
+
+            }
+            Player.STATE_READY -> {
+                Log.d(TAG, "onPlaybackStateChanged: ${TimeUnit.MILLISECONDS.toSeconds(player.duration)}")
+            }
+        }
+    }
+
+    //get Title from metadata
+    override fun onMediaMetadataChanged(mediaMetadata: MediaMetadata) {
+
+
+    }
+
+    // save details if Activity is destroyed
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        Log.d(TAG, "onSaveInstanceState: " + player.currentPosition)
+        // current play position
+        outState.putLong("SeekTime", player.currentPosition)
+        // current mediaItem
+        outState.putInt("mediaItem", player.currentMediaItemIndex)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onSaveInstanceState: " + player.currentPosition)
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
     }
 }
