c3ee1b954aab7e83dd2dbc893621c6aacf8ec9fd
huy9797 <huyphamquang1997@gmail.com>
2022-07-21 02:09:11 +0700

    yoda

---
+++ app/src/main/java/com/example/exoplayerdemo/notify/AlarmBroadcast.kt
@@ -0,0 +1,82 @@
+package com.example.exoplayerdemo.notify
+
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.media.RingtoneManager
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat.getSystemService
+import com.example.exoplayerdemo.R
+
+class AlarmBroadcast : BroadcastReceiver() {
+
+    companion object {
+        const val NOTIFY_ID = "NOTIFY_ID"
+    }
+
+    private lateinit var notificationManager: NotificationManager
+    private var notifyId = -1
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        intent?.let {
+            notifyId = it.getIntExtra(NOTIFY_ID, -1)
+        }
+        context?.let {
+            createChannel(it)
+            getNotificationManager(it)
+            if (notifyId != -1)
+                notificationManager.notify(notifyId, buildNotification(it))
+        }
+    }
+
+    private fun createChannel(context: Context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel =
+                NotificationChannel(
+                    TimerService.CHANNEL_ID,
+                    "Timer",
+                    NotificationManager.IMPORTANCE_DEFAULT
+                )
+            notificationChannel.setSound(
+                RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION),
+                null
+            )
+            notificationChannel.setShowBadge(true)
+            val notificationManager = context.getSystemService(NotificationManager::class.java)
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+    }
+
+    private fun getNotificationManager(context: Context) {
+        notificationManager = getSystemService(
+            context,
+            NotificationManager::class.java
+        ) as NotificationManager
+    }
+
+    private fun buildNotification(context: Context): Notification {
+        val title = "Sắp có lịch học"
+        val content = "Học sinh ${notifyId + 1}"
+
+        val intent = Intent(context, NotifyActivity::class.java)
+        val pIntent = PendingIntent.getActivity(context, -1, intent, PendingIntent.FLAG_ONE_SHOT)
+
+        return NotificationCompat.Builder(context, TimerService.CHANNEL_ID)
+            .setContentTitle(title)
+            .setOngoing(true)
+            .setContentText(content)
+            .setColorized(true)
+            .setColor(Color.parseColor("#BEAEE2"))
+            .setSmallIcon(R.drawable.ic_clock)
+            .setOnlyAlertOnce(false)
+            .setContentIntent(pIntent)
+            .setAutoCancel(true)
+            .build()
+    }
+}
--- app/src/main/java/com/example/exoplayerdemo/notify/NotifyActivity.kt
+++ app/src/main/java/com/example/exoplayerdemo/notify/NotifyActivity.kt
@@ -1,7 +1,12 @@
 package com.example.exoplayerdemo.notify
 
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.os.SystemClock
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.exoplayerdemo.R
 
@@ -9,9 +14,39 @@ class NotifyActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_notify)
+        val button = findViewById<Button>(R.id.button)
 
-        val timerService = Intent(this, TimerService::class.java)
-        timerService.putExtra(TimerService.UPCOMING_CLASS_TIME, 20L)
-        startService(timerService)
+//        val timerService = Intent(this, TimerService::class.java)
+//        timerService.putExtra(TimerService.UPCOMING_CLASS_TIME, 20L)
+//        startService(timerService)
+
+        button.setOnClickListener {
+            setAlarm(0, 6 * 1000)
+            setAlarm(1, 10 * 1000)
+        }
+
+    }
+
+    private fun setAlarm(id: Int, long: Long) {
+        val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        val intent = Intent(this, AlarmBroadcast::class.java)
+        intent.putExtra(AlarmBroadcast.NOTIFY_ID, id)
+        val pendingIntent =
+            PendingIntent.getBroadcast(this, id, intent, 0)
+        alarmManager.set(
+            AlarmManager.RTC_WAKEUP,
+            SystemClock.elapsedRealtime() + long,
+            pendingIntent
+        )/*
+        alarmManager.setExact(
+            AlarmManager.RTC_WAKEUP,
+            SystemClock.elapsedRealtime() + 10000,
+            pendingIntent
+        )
+        alarmManager.setExact(
+            AlarmManager.RTC_WAKEUP,
+            SystemClock.elapsedRealtime() + 15000,
+            pendingIntent
+        )*/
     }
 }
